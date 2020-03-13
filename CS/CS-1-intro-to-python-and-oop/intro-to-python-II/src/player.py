e3dca8df4fddb1da5030937137341dc9c2642ee6
Trevor Clack <tclack@aol.com>
2020-03-12 17:48:13 -0700

    MVP for adventure game

--- CS/CS-1-intro-to-python-and-oop/intro-to-python-II/src/player.py
+++ CS/CS-1-intro-to-python-and-oop/intro-to-python-II/src/player.py
@@ -1,7 +1,48 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+import os
+import time
+
 
 class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.items = [] # begin a blank list to store items
+
+    def show_inventory(self):
+        """ Display items the player has taken"""
+        if len(self.items) == 0:
+            print("No Items in Inventory")
+        else:
+            print("Inventory Items:")
+        for item in self.items:
+            print(f'{item} ::: {item.description}\n')
+
+    def gain_item(self, item):
+        """ Adds item to inventory, removes from room,
+        perform relevant error checks """
+        os.system('clear')
+        if item:
+            self.current_room.items.remove(item)
+            self.items.append(item)
+            print(f'Added {item} to inventory')
+            print(item.description)
+            time.sleep(1)
+        else:
+            print(f'{item} does not appear to be an item in this room')
+            time.sleep(1)
+
+    def lose_item(self, item):
+        """Removes item to inventory, Adds to room,
+        perform relevant error checks """
+        os.system('clear')
+        try:
+            self.items.remove(item)
+            self.current_room.items.append(item)
+            print(f"""{item.name} bequeathed to the {self.current_room.name}\n
+                    so long for now {item}""")
+            time.sleep(1)
+        except:
+            print(f"You can't lose what you don't have...({item} not in your inventory)")
+            time.sleep(1)
--- CS/CS-1-intro-to-python-and-oop/intro-to-python-II/src/room.py
+++ CS/CS-1-intro-to-python-and-oop/intro-to-python-II/src/room.py
@@ -5,5 +5,6 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items = [] # Initialize room with empty item list
 
     n_to, s_to, e_to, w_to = None, None, None, None
