8564a736d76d441e0e0f04045fe8ea483822a797
Trevor Clack <tclack@aol.com>
2020-03-17 18:39:45 -0700

    completed 2nd day MVP, did some stretch challenges with searching and recursvive sorting. Added self-appointed stretches as well -- insertion sort for iterative sorting and quick sort for recursive sorting

--- CS/CS-2-Algorithms/1-sorting/src/recursive_sorting/merge_sort_test.py
+++ CS/CS-2-Algorithms/1-sorting/src/recursive_sorting/merge_sort_test.py
@@ -1,4 +1,11 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
+#!/usr/bin/env python3
+
+from random import seed, shuffle
+
+seed(5)
+arr = [1,1,2,3,5,8,13,21,34]
+shuffle(arr)
+
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
@@ -35,23 +42,5 @@ def merge_sort( arr ):
     sorted_arr = merge_sort(sorted_arr)
     return sorted_arr
 
-
-#print(merge_sort(arr))
-
-# STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
-    return arr
-
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
-
-    return arr
-
-
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
-
-    return arr
+print('mixed:',arr)
+print(merge_sort(arr))
--- CS/CS-2-Algorithms/1-sorting/src/searching/searching.py
+++ CS/CS-2-Algorithms/1-sorting/src/searching/searching.py
@@ -1,30 +1,51 @@
 # STRETCH: implement Linear Search				
+#arr = [0,1,2,3,4,5,6,7,8,9,10]
+#target = 5
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
+    for i, t in enumerate(arr):
+        if t == target:
+            return i
+    return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1 # array empty
+      
+    low = 0
+    high = len(arr) - 1
 
-  # TO-DO: add missing code
-
-  return -1 # not found
+    t = ''
+    while t != target:
+        middle = (high - low) // 2
+        if arr[middle] == target:
+            return middle
+        elif target < middle:
+            high = middle
+        elif target > middle:
+            low = middle 
+    return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
-def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+def binary_search_recursive(arr, target, low=None, high=None):
+    if low == None:
+        low = 0
+    if high == None:
+        high = len(arr)-1
+    
+    middle = (low + high) // 2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    if len(arr) == 0:
+        return -1 # array empty
+    if arr[middle] == target:
+        return middle
+    elif arr[middle] > target:
+        high = middle
+        return binary_search_recursive(arr, target, low, high)
+    elif arr[middle] < target:
+        low = middle
+        return binary_search_recursive(arr, target, low, high)
+    return -1 # not found
