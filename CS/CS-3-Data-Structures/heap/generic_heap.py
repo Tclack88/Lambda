8f54e168bcb6133b45706ae561743f570f203865
Trevor Clack <tclack@aol.com>
2020-03-28 11:55:47 -0700

    completed generic heaps. tried max heap, but some tests failed

--- CS/CS-3-Data-Structures/heap/generic_heap.py
+++ CS/CS-3-Data-Structures/heap/generic_heap.py
@@ -1,7 +1,10 @@
 class Heap:
     def __init__(self, comparator=None):
         self.storage = []
-        self.comparator = comparator
+        if comparator == None:
+            self.comparator = lambda x, y: x > y
+        else:
+            self.comparator = comparator
 
     def insert(self, value):
         self.storage.append(value)
@@ -11,7 +14,6 @@ class Heap:
         top = self.storage.pop(0)
         self.storage.insert(0, None)
         self._sift_down(0)
-        print('after delete:',self.storage)
         return top
 
     def get_priority(self):
@@ -24,11 +26,8 @@ class Heap:
     #### Problem with bubble up for deletion
 
     def _bubble_up(self, index):
-        print('bubbling up',index)
-        print(self.get_size())
         has_priority = False
-        if index > 1:
-            has_priority = self.comparator(self.storage[index], self.storage[(index - 1) //2])
+        has_priority = self.comparator(self.storage[index], self.storage[(index - 1) //2])
         while has_priority and index > 0:
             parent_index = (index - 1) // 2
             child = self.storage[index]
@@ -38,32 +37,41 @@ class Heap:
             self.storage.insert(index, parent)
             index = (index - 1) // 2
             has_priority = self.comparator(self.storage[index], self.storage[(index - 1) //2])
-        print(self.storage)
 
 
     def _sift_down(self, index):
         child_indices = 2 * index + 1, 2 * index + 2
-        try:
+        try:  # first check if both child indices exist
             if self.storage[child_indices[0]] and self.storage[child_indices[1]]:
-                if self.storage[child_indices[0]] < self.storage[child_indices[1]]:
+                if self.comparator(self.storage[child_indices[0]], self.storage[child_indices[1]]):
                     child_index = child_indices[0]
                 else:
                     child_index = child_indices[1]
-            elif self.storage[child_indices[0]] or self.storage[child_indices[1]]:
+            elif self.storage[child_indices[0]]: # if only one exists, it must be the 1st
                 child_index = child_indices[0]
-            #else:
-            #    self.storage.pop(index)
-            #    return
-            child = self.storage[child_index]
+            else:
+                self.storage.pop(index) # Otherwise, remove the element, you're likely
+            child = self.storage[child_index]  # at the end
             self.storage[index] = child
             self.storage[child_index] = None
             self._sift_down(child_index)
         except IndexError:
             self.storage.pop(index)
+            if len(self.storage) > 0: # if the list isn't empty, this exception occurs
+                val_append = self.storage.pop(-1) # if there are no children for the
+                self.storage.insert(index,val_append) # node, but there exist "lower tier"
+                                         # values, so bring one up to avoid missing sift
 
+# Eg. of final statement:
+"""
+        10                                  8
+     8      5                           7      5
+   6  7   1   2     -- remove 10 -->  6  ?   1   2 
+  5                                  5 --^
+"""
 
-
-heap = Heap(lambda x, y: x < y)
+"""
+heap = Heap(lambda x, y: x > y)
 
 heap.insert(6)
 heap.insert(7)
@@ -75,3 +83,4 @@ heap.insert(2)
 heap.insert(5)
 
 print(heap.storage)
+"""
--- CS/CS-3-Data-Structures/heap/max_heap.py
+++ CS/CS-3-Data-Structures/heap/max_heap.py
@@ -3,19 +3,44 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        print('inserting....',value)
+        self._bubble_up(self.get_size() - 1)
 
     def delete(self):
-        pass
+        self.storage[0] = None
+        self._sift_down(0)
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        print('bubble up')
+        print(self.storage)
+        if index == 0:
+            return # nothing to bubble up, this is the 1st item
+        parent_index = (index - 1) // 2
+        if self.storage[parent_index] < self.storage[index] and index > 0:
+            self.storage[parent_index], self.storage[index] = self.storage[index], self.storage[parent_index]
+            self._bubble_up(parent_index)
+        
 
     def _sift_down(self, index):
-        pass
+        print('sifting down')
+        if self.get_size() < 2* index + 2:
+            self.storage.pop(index)
+            return
+        elif self.get_size() < 2 * index + 2:
+            child_index = 2 * index + 1
+        else:
+            child_indices = [2 * index + 1, 2 * index + 2]
+            if self.storage[child_indices[0]] > self.storage[child_indices[1]]:
+                child_index = child_indices[0]
+            else:
+                child_index = child_indices[1]
+        self.storage[index], self.storage[child_index] = self.storage[child_index],self.storage[index]
+        self._sift_down(child_index)
+        print(self.storage)   
