3ec7af48cfc63c0e4bed1ded4b76f6a36e29e020
Trevor Clack <tclack@aol.com>
2020-03-28 21:35:32 -0700

    completed max heap

--- CS/CS-3-Data-Structures/heap/max_heap.py
+++ CS/CS-3-Data-Structures/heap/max_heap.py
@@ -4,12 +4,13 @@ class Heap:
 
     def insert(self, value):
         self.storage.append(value)
-        print('inserting....',value)
         self._bubble_up(self.get_size() - 1)
 
     def delete(self):
-        self.storage[0] = None
+        top = self.storage.pop(0)
+        self.storage.insert(0, None)
         self._sift_down(0)
+        return top
 
     def get_max(self):
         return self.storage[0]
@@ -18,8 +19,6 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        print('bubble up')
-        print(self.storage)
         if index == 0:
             return # nothing to bubble up, this is the 1st item
         parent_index = (index - 1) // 2
@@ -29,11 +28,13 @@ class Heap:
         
 
     def _sift_down(self, index):
-        print('sifting down')
-        if self.get_size() < 2* index + 2:
+        if self.get_size() < 2* index + 3: # if there's no children below this index
             self.storage.pop(index)
+            if self.get_size() > 0: # If this is not the last index. Fill the 'hole'
+                last = self.storage.pop(-1) # with the last element, otherwise, the array
+                self.storage.insert(index,last) # may lose order
             return
-        elif self.get_size() < 2 * index + 2:
+        elif self.get_size() == 2 * index + 2:
             child_index = 2 * index + 1
         else:
             child_indices = [2 * index + 1, 2 * index + 2]
@@ -43,4 +44,3 @@ class Heap:
                 child_index = child_indices[1]
         self.storage[index], self.storage[child_index] = self.storage[child_index],self.storage[index]
         self._sift_down(child_index)
-        print(self.storage)   
