6b436a91928ef067cd506583a8d01f2995b96917
tclack88 <--global>
2020-04-16 21:54:14 -0700

    MVP for sprint challenge

--- CS/CS-5-Graphs/sc/adv.py
+++ CS/CS-5-Graphs/sc/adv.py
@@ -25,11 +25,43 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
 
+def move_to_front(direction, dir_list):
+    # Helper function moves last direction to front of dir_list
+    # The strategy here is if we enter a room from a certain direction
+    # we want to guarantee we search all other directions before returning
+    # back the way we came
+    dir_map = dict(zip(list('nsew'),list('snwe')))
+    opposite = dir_map[direction]
+    dir_list.remove(opposite)
+    dir_list.insert(0,opposite)
+    return dir_list
 
+visited = {} # empty dictionary to keep track of available rooms left to travel
+traversal_path = [] # Fill this out with directions to walk
+# initialize with starting room
+exits = player.current_room.get_exits()
+current_room = player.current_room.id
+visited[current_room] = exits # available directions
+
+possible =  len(exits) # number of untraveled directions from current room
+while possible:
+    direction = visited[current_room].pop()
+    player.travel(direction)
+    traversal_path.append(direction) # add moved direction to plan
+    current_room = player.current_room.id
+    been_here = visited.get(current_room, False) # in visited
+    #print('been here results:', been_here)
+    if been_here == False:
+        exits = player.current_room.get_exits()
+        possible =  len(exits) # Technically unnecessary, you can always reverse
+        exits = move_to_front(direction, exits) #lower priority of incoming dir 
+        visited[current_room] = exits # fill new room with possible directions
+    else:
+        exits = visited[current_room]
+        possible = len(exits)
+
+#print(traversal_path)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,6 +83,7 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
+"""
 player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
@@ -60,3 +93,4 @@ while True:
         break
     else:
         print("I did not understand that command.")
+"""
