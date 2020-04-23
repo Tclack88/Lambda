e84bff1553586747f80c733253c4a3ef7495656e
Trevor Clack <tclack@aol.com>
2020-04-22 18:16:03 -0700

    Added stack methods to cpu emulator. Refactored other definitions to be a bit smaller by handling operands and program counter incrementing in run method to stay DRY

--- CS/CS-6-Computer-Architecture/ls8/cpu.py
+++ CS/CS-6-Computer-Architecture/ls8/cpu.py
@@ -10,12 +10,16 @@ class CPU:
         self.pc = 0 # process counter
         self.ram = [0]*256 # Initialize 8 byte ram (array of 0's)
         self.reg = [0] * 8 # Initialize register values (array of 0's)
+        self.sp = 7 # stack pointer, set it to last (arbitrary) spot in register
+        self.reg[self.sp] = 0xf4
         # map the bytes to a class method
         self.instruction_map = {
-            0b10000010: self.handle_LDI, # LDI (load instruction)
-            0b00000001: self.handle_HLT, # HLT (Hault)
-            0b01000111: self.handle_PRN, # PRN (print)
-            0b10100010: self.handle_MUL  # MUL (multiply)
+            0b10000010: self.handle_LDI,  # LDI (load instruction)
+            0b00000001: self.handle_HLT,  # HLT (Hault)
+            0b01000111: self.handle_PRN,  # PRN (print)
+            0b10100010: self.handle_MUL,  # MUL (multiply)
+            0b01000101: self.handle_PUSH, # PUSH (push)
+            0b01000110: self.handle_POP   # POP (pop)
             }
 
 
@@ -37,7 +41,6 @@ class CPU:
                 # append to ram
                 self.ram_write(address, int(instruction, 2))
                 address += 1
-                 
     
     def ram_read(self, address):
         return self.ram[address]
@@ -73,35 +76,58 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
-    def handle_LDI(self):
-        adr = self.ram_read(self.pc + 1)
-        val = self.ram_read(self.pc + 2)
+    def handle_LDI(self, adr, val):
+        """Set register address to value (given at self.pc + 1 and +2  respectively"""
         self.reg[adr] = val # set specified register variable to value
-        self.pc += 3
 
-    def handle_PRN(self):
-        val = self.reg[self.ram_read(self.pc + 1)]
+    def handle_PRN(self, register, _):
+        """print value stored in register (given in self.pc + 1"""
+        val = self.reg[register]
         print(val)
-        self.pc += 2
 
-    def handle_HLT(self):
+    def handle_HLT(self, *args):
+        """Exit the program"""
         print('program exiting')
         exit()
 
-    def handle_MUL(self):
-        val1 = self.ram_read(self.pc + 1)
-        val2 = self.ram_read(self.pc + 2)
-        self.pc += 3
+    def handle_MUL(self, val1, val2):
+        """ take val 1 and val 2 (at self.pc +1 and + 2 respectively)
+        pass to alu for operation """
         self.alu('MUL',val1, val2)
+
+    def handle_PUSH(self, register, _):
+        """Increment stack pointer, place registry value onto stack"""
+        self.reg[self.sp] += 1 # increment stack pointer
+        value = self.reg[register] # get register to be moved 
+        address = self.reg[self.sp] # Get address of newly incremented stack
+                                    # pointer where we will write
+        self.ram_write(address, value)
+
+    def handle_POP(self, register, _):
+        """pop item from stack into register, decrement stack pointer
+        this function accepts the register were we will store from the top of the stack"""
+        address = self.reg[self.sp] # Get address on top of stack
+        value = self.ram_read(address) # Get value stored at that address
+        self.reg[register] = value # move value into specified register
+        self.reg[self.sp] -= 1 # decrement stack pointer
+        
 
     def run(self):
         """Run the CPU."""
         while True:
             ir = self.ram_read(self.pc)
             # map ir to function and execute
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b= self.ram_read(self.pc + 2)
             function = self.instruction_map[ir]
-            if function == None:
-                print(f'Instruction: {int(ir, 2)}, not understood')
+            if function == None: # handle errors without silence
+                print(f'Instructiona: {int(ir, 2)}, not understood')
                 self.instruction_map[0] # call exit instruction
-            function()
+            function(operand_a,operand_b)
+
+            # instruction length stored as top most significant bits
+            # performing a logical "and" to get those and shifting over
+            # returns the amount we shift to get to next instruction
+            instr_len = ((ir & 0b11000000) >> 6) + 1
+            self.pc += instr_len
 
