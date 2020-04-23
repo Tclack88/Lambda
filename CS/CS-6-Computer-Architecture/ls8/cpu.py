16084e6ba17d6ad859dc47d9c1c5e2c2da11c1c8
Trevor Clack <tclack@aol.com>
2020-04-23 15:47:55 -0700

    added Call and Return functions to cpu. Implemented a flag for a bit to determine whether a 'jump-like' command is called to determine whether or not to increase the stack pointer

--- CS/CS-6-Computer-Architecture/ls8/cpu.py
+++ CS/CS-6-Computer-Architecture/ls8/cpu.py
@@ -11,15 +11,19 @@ class CPU:
         self.ram = [0]*256 # Initialize 8 byte ram (array of 0's)
         self.reg = [0] * 8 # Initialize register values (array of 0's)
         self.sp = 7 # stack pointer, set it to last (arbitrary) spot in register
+        self.fl = 0 # flag (indicating if a jump of some sort is happening)
         self.reg[self.sp] = 0xf4
         # map the bytes to a class method
         self.instruction_map = {
             0b10000010: self.handle_LDI,  # LDI (load instruction)
             0b00000001: self.handle_HLT,  # HLT (Hault)
             0b01000111: self.handle_PRN,  # PRN (print)
+            0b10100000: self.handle_ADD,  # ADD (add)
             0b10100010: self.handle_MUL,  # MUL (multiply)
             0b01000101: self.handle_PUSH, # PUSH (push)
-            0b01000110: self.handle_POP   # POP (pop)
+            0b01000110: self.handle_POP,  # POP (pop)
+            0b01010000: self.handle_CALL, # CALL (call)
+            0b00010001: self.handle_RET   # RET (return)
             }
 
 
@@ -90,6 +94,11 @@ class CPU:
         print('program exiting')
         exit()
 
+    def handle_ADD(self, val1, val2):
+        """ pass on val1 and val2 (at self.pc +1 and + 2 respectively) 
+        to alu for operation"""
+        self.alu('ADD', val1, val2)
+
     def handle_MUL(self, val1, val2):
         """ take val 1 and val 2 (at self.pc +1 and + 2 respectively)
         pass to alu for operation """
@@ -110,7 +119,17 @@ class CPU:
         value = self.ram_read(address) # Get value stored at that address
         self.reg[register] = value # move value into specified register
         self.reg[self.sp] -= 1 # decrement stack pointer
-        
+    
+    def handle_CALL(self, register, _):
+        self.reg[self.sp] -= 1 # decrement stack pointer (prepare to push)
+        self.ram_write(self.reg[self.sp], self.pc + 2) # push return addr
+        self.pc = self.reg[register] # change pc to value specified in register
+
+    def handle_RET(self, *args):
+        """to last instruction on the stack"""
+        # set return address to pc from top of stack
+        self.pc = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1 # increment stack pointer (i.e. pop off top)
 
     def run(self):
         """Run the CPU."""
@@ -118,7 +137,7 @@ class CPU:
             ir = self.ram_read(self.pc)
             # map ir to function and execute
             operand_a = self.ram_read(self.pc + 1)
-            operand_b= self.ram_read(self.pc + 2)
+            operand_b = self.ram_read(self.pc + 2)
             function = self.instruction_map[ir]
             if function == None: # handle errors without silence
                 print(f'Instructiona: {int(ir, 2)}, not understood')
@@ -129,5 +148,9 @@ class CPU:
             # performing a logical "and" to get those and shifting over
             # returns the amount we shift to get to next instruction
             instr_len = ((ir & 0b11000000) >> 6) + 1
-            self.pc += instr_len
+            self.fl = ((ir & 0b00010000) >> 4)
+            if self.fl:
+                self.fl = 0 # reset
+            else: # ignore flag and increment pc
+                self.pc += instr_len
 
